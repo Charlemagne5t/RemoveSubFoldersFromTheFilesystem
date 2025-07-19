f7ce6ffc1cee00a273fb6e38e2e72c6e6dbabc17
alek <wait82985@gmail.com>
2025-07-19 13:40:08 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,52 +1,48 @@
 import java.util.*;
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        List<List<StringIndex>> list = new ArrayList<>();
-        int n = folder.length;
-
-        for(int i = 0; i < n; i++) {
-            list.add(new ArrayList<>());
-            String[] fol = folder[i].split("/");
-            for(int j = 1; j < fol.length; j++) {
-                list.get(i).add(new StringIndex(fol[j], i));
-            }
-        }
-        list.sort(Comparator.comparing(List::size));
         List<String> res = new ArrayList<>();
         Trie trie = new Trie();
-        for(int i = 0; i < n; i++) {
-            if(trie.insert(list.get(i))){
-                res.add(folder[list.get(i).get(0).index] );
-            }
+        for(int i = 0; i < folder.length; i++){
+            trie.insert(folder[i], i);
         }
+        dfs(res, folder, trie.root);
         return res;
+    }
+    void dfs(List<String> res, String[] folder, TrieNode root){
+        if(root.isTerminal){
+            res.add(folder[root.index]);
+        }
+        for(TrieNode child : root.children.values()){
+            dfs(res, folder, child);
+        }
+
     }
 }
 class Trie {
     TrieNode root;
 
     public Trie() {
-        root = new TrieNode();
+        root = new TrieNode("", -1);
     }
 
-    public boolean insert(List<StringIndex> path) {
-        int pathLength = path.size();
+    public void insert(String path, int index) {
+        String[] dirs = path.split("/");
         TrieNode temp = root;
-        for (int i = 0; i < pathLength; i++) {
-            if(!temp.children.containsKey(path.get(i).str)){
-                TrieNode node = new TrieNode();
-                temp.children.put(path.get(i).str, node);
-            } else {
-                if(temp.children.get(path.get(i).str).isTerminal) {
-                    return false;
-                }
+        for (int i = 0; i < dirs.length; i++) {
+            if(!temp.children.containsKey(dirs[i])){
+                TrieNode node = new TrieNode(dirs[i], index);
+                temp.children.put(dirs[i], node);
+            }else if(temp.children.containsKey(dirs[i]) && temp.children.get(dirs[i]).isTerminal){
+                return;
             }
-            temp = temp.children.get(path.get(i).str);
-            if(i == pathLength - 1){
+            temp = temp.children.get(dirs[i]);
+            if(i == dirs.length - 1){
                 temp.isTerminal = true;
+                temp.children = new HashMap<>();
+                temp.index = index;
             }
         }
-        return true;
 
     }
 
@@ -54,29 +50,13 @@ class Trie {
 }
 
 class TrieNode{
-
+    String value;
     Map<String, TrieNode> children = new HashMap<>();
     boolean isTerminal;
-
-    public TrieNode() {
-
-        this.isTerminal = false;
-    }
-}
-class StringIndex {
-    String str;
-    int index;
-
-    public StringIndex(String str, int index) {
-        this.str = str;
+    int index = -1;
+    public TrieNode(String value, int index) {
+        this.value = value;
         this.index = index;
-    }
-
-    @Override
-    public String toString() {
-        return "StringIndex{" +
-                "str='" + str + '\'' +
-                ", index=" + index +
-                '}';
+        this.isTerminal = false;
     }
 }
